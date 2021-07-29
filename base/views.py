4230693d589c570c3dfd3b0d8172554a151ae272
Xperhub <iyandataofeek11@gmail.com>
2021-07-29 22:56:10 +0100

    Finish up To Do List

--- base/views.py
+++ base/views.py
@@ -79,10 +79,6 @@ class TaskUpdate(LoginRequiredMixin, UpdateView):
     fields = fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(TaskCreate, self).form_valid(form)
-
 class TaskDelete(LoginRequiredMixin,DeleteView):
     model = Task
     context_object_name = 'task'
